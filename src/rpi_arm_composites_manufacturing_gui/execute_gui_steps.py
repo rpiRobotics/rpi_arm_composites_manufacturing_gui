d2be6db9d7e38af5d0491de42c12715785f242fa
wlawler45 <williamgmlawler@yahoo.com>
2019-03-26 17:44:46 -0400

    highbay upload after most recent experiment

--- src/rpi_arm_composites_manufacturing_gui/execute_gui_steps.py
+++ src/rpi_arm_composites_manufacturing_gui/execute_gui_steps.py
@@ -92,7 +92,7 @@ class GUI_Step_Executor(QObject):
 
     def _execute_steps(self,steps_index, target="",target_index=-1):
         #TODO Create separate thread for each execution step that waits until in_process is true
-        
+        #TODO try with done cb
         def send_action(goal):
             self.client_handle=self.client.send_goal(goal,feedback_cb=self._feedback_receive)
             #self.client_handle=self.client.send_goal(goal,feedback_cb=self._feedback_receive,done_cb=self._next_command)
@@ -225,6 +225,7 @@ class GUI_Step_Executor(QObject):
         #g=ProcessStepGoal(self.execute_states[6][0], "")
         #self.client_handle=self.client.send_goal(g,feedback_cb=self._feedback_receive)
         client.cancel_all_goals()
+        #TODO make it so it checks previous goal handle has returned before reenabling
         self.recover_from_pause=True
 
     def _publish_state_message(self):
